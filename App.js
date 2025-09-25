9af28562f58938fb626cc30c29d6052144408995
denudes7 <daviddenis0112@gmail.com>
2025-09-25 18:36:35 -0300

    Criando frontend com React Native e Expo

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginRegistrationScreen from './screens/LoginRegistrationScreen';
+// import HomeScreen from './screens/HomeScreen';
+// import QRCodeScreen from './screens/QRCodeScreen';
+// import ChecklistScreen from './screens/ChecklistScreen';
+// import ProfileScreen from './screens/ProfileScreen';
+
+export default function App() {
+  // Para testar a tela de login/registro, descomente a linha abaixo
+  return <LoginRegistrationScreen />;
+
+  // Para testar outras telas, comente a linha acima e descomente a tela que quer ver
+  // return <HomeScreen />;
+  // return <QRCodeScreen />;
+  // return <ChecklistScreen />;
+  // return <ProfileScreen />;
+}
